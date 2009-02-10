bb5ebcc13af45e7b94bd587cb0519f6d9658dc7c
Mohsin Hijazee <mh@dedomenon.org>
2009-02-10 18:50:10 +0500

    Added AGPL header

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,3 +1,22 @@
+################################################################################
+#This file is part of Dedomenon.
+#
+#Dedomenon is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Dedomenon is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License
+#along with Dedomenon.  If not, see <http://www.gnu.org/licenses/>.
+#
+#Copyright 2008 Raphaël Bauduin
+################################################################################
+
 # tell the I18n library where to find your translations
 I18n.load_path += Dir[ File.join(RAILS_ROOT, 'config', 'locale', '*.{rb,yml}') ]
 
